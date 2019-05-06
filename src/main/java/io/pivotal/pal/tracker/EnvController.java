ddfb30072049e1daf7d77a33e6dc2fccd38dee78
Chris Bain <baincd@users.noreply.github.com>
2019-05-06 14:15:55 -0600

    Second Assignment

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,42 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    private String port;
+    private String memLim;
+    private String cfInstanceIdx;
+    private String cfInstanceAddr;
+
+    public EnvController(@Value("${port:NOT SET}") String port,
+                         @Value("${memory.limit:NOT SET}") String memLim,
+                         @Value("${cf.instance.index:NOT SET}") String cfInstanceIdx,
+                         @Value("${cf.instance.addr:NOT SET}") String cfInstanceAddr) {
+        this.port = port;
+        this.memLim = memLim;
+        this.cfInstanceIdx = cfInstanceIdx;
+        this.cfInstanceAddr = cfInstanceAddr;
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+        Map map = new HashMap();
+        map.put("PORT",port);
+        map.put("MEMORY_LIMIT",memLim);
+        map.put("CF_INSTANCE_INDEX",cfInstanceIdx);
+        map.put("CF_INSTANCE_ADDR",cfInstanceAddr);
+        return map;
+    }
+
+    @GetMapping("/stop")
+    public void stop() {
+        System.exit(1);
+    }
+}
